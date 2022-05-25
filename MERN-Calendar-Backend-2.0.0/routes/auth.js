18e4514b7d798aa7a913018f342f3ab75ba2ebb9
kevin-monat <kmorenonatera@gmail.com>
2022-05-25 12:38:24 -0600

    Modificacion de Rutas
    Se agrego la ruta para agregar usuarios de medicos. Ademas, tambien se personalizaron los nombres de los URLs.

--- MERN-Calendar-Backend-2.0.0/routes/auth.js
+++ MERN-Calendar-Backend-2.0.0/routes/auth.js
@@ -5,23 +5,33 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { crearUsuarioPaciente, loginUsuario, revalidarToken, crearUsuarioMedico } = require('../controllers/auth');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 
 const router = Router();
 
 
-
 router.post(
-    '/new', 
+    '/newPatient', 
     [ // middlewares
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
         validarCampos
     ],
-    crearUsuario 
+    crearUsuarioPaciente 
+);
+
+router.post(
+    '/newDoctor', 
+    [ // middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    crearUsuarioMedico 
 );
 
 router.post(
