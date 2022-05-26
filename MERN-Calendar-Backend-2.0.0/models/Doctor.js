f6849a2ca08700684d1e7dd1a913b4e7a1311067
Pabelocko <105995600+Pabelocko@users.noreply.github.com>
2022-05-26 12:57:33 -0600

    campos doc
    ok maguey

--- MERN-Calendar-Backend-2.0.0/models/Doctor.js
+++ MERN-Calendar-Backend-2.0.0/models/Doctor.js
@@ -3,6 +3,20 @@ const { Schema, model } = require('mongoose');
 const DoctorSchema = Schema({
     name: {
         type: String,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    license: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    speciality: {
+        type: String,
         required: true
     },
     email: {
