2bc716df8c21450ab5b18a55caad50222f7e731b
alnair gonz <alnairgl1999@hotmail.com>
2022-06-13 11:49:58 -0600

    elegir según los eventos

--- MERN-Calendar-Front-3.0.0/src/components/calendar/CalendarEvent.js
+++ MERN-Calendar-Front-3.0.0/src/components/calendar/CalendarEvent.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 export const CalendarEvent = ({ event }) => {
 
+    const { uid } = useSelector( state => state.auth );
     const { title, user } = event;
 
+    console.log(event.user._id, uid);
+
+    if(event.user._id != uid){
+        return ( 
+        <div> 
+            
+        </div> 
+        )
+    }
+    else{
+    
     return (
         <div>
+        
             <strong> { title } </strong>
             {<span> { user.name } </span>}
         </div>
     )
+    }
 }
