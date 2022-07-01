f2aaf0b233cb199ddf5d0ef7e8cc36946f072a80
Pabelocko <105995600+Pabelocko@users.noreply.github.com>
2022-07-01 10:23:23 -0600

    center
    cent

--- MERN-Calendar-Front-3.0.0/src/components/mapa/MapView.js
+++ MERN-Calendar-Front-3.0.0/src/components/mapa/MapView.js
@@ -9,7 +9,7 @@ import "leaflet/dist/leaflet.css";
 
 const MapView = (props) => {
   const [state, setState] = useState({
-    currentLocation: { lat: 52.52437, lng: 13.41053 },
+    currentLocation: { lat: 28.6610353, lng: -106.0676981 },
     zoom: 13,
     data,
   });
