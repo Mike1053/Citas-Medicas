7ed54ac2208897bee6248b4b052c4620d414f68b
Pabel Hernández <pabel.hernandez@live.com>
2022-07-15 08:35:29 -0600

    ajustes
    x

--- nodejs-leaflet-socketio-master/src/public/js/main.js
+++ nodejs-leaflet-socketio-master/src/public/js/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map-template').setView([51.505, -0.09], 3);
+var map = L.map('map-template').setView([28.6802101, -106.0865403], 10);
 
 const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
 const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
@@ -8,9 +8,10 @@ const tile = L.tileLayer(tileURL2);
 // Socket Io
 const socket = io.connect();
 
-// Marker
-const marker = L.marker([50.5, 30.5]); // kiev, ukraine
-marker.bindPopup('Hello There!');
+// Marker 00
+
+const marker = L.marker([28.653784417300734, -106.06957097047322]); //Hospital general Chihuahua Mex.
+marker.bindPopup('Hospital General de Chihuahua');
 map.addLayer(marker);
 
 // Geolocation
@@ -18,7 +19,7 @@ map.locate({enableHighAccuracy: true})
 map.on('locationfound', (e) => {
   const coords = [e.latlng.lat, e.latlng.lng];
   const newMarker = L.marker(coords);
-  newMarker.bindPopup('You are Here!');
+  newMarker.bindPopup('Tú estas aquí!');
   map.addLayer(newMarker);
   socket.emit('userCoordinates', e.latlng);
 });
@@ -27,13 +28,13 @@ map.on('locationfound', (e) => {
 socket.on('newUserCoordinates', (coords) => {
   console.log(coords);
   const userIcon = L.icon({
-    iconUrl: '/img/icon2.png',
+    iconUrl: '/img/icon3.png',
     iconSize: [38, 42],
   })
   const newUserMarker = L.marker([coords.lat, coords.lng], {
     icon: userIcon 
   });
-  newUserMarker.bindPopup('New User!');
+  newUserMarker.bindPopup('Nuevo Médico Conectado!');
   map.addLayer(newUserMarker);
 }); 
 
